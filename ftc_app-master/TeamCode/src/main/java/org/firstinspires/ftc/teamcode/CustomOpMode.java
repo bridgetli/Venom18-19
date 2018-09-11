fab85246bbbd3247d5d447e9c87aacebd0a128e9
viperbots <31671851+viperbots@users.noreply.github.com>
2018-09-11 16:36:06 -0500

    fixed heiarchy

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomOpMode.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class CustomOpMode extends OpMode{
+    //drive motors
+    DcMotor motorFR;
+    DcMotor motorFL;
+    DcMotor motorBR;
+    DcMotor motorBL;
+
+    //winch motors???
+    DcMotor motorWinchUp;
+    DcMotor motorWinchDown;
+
+
+    final double winchDownPower = .5;
+    final double winchUpPower = .5;
+
+    Servo servoMarker;
+
+    final double servoMarkerStartPos = 1;
+    final double servoMarkerEndPos = 0;
+
+    IMU imu;
+
+    //just had to put these to run the code dw about it
+    public void init() {
+
+    }
+    public void loop() {
+        
+    }
+
+
+
+    // initzialization method
+    public void initizialize() {
+        motorFR = hardwareMap.dcMotor.get("motorFR");
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+
+        motorWinchUp = hardwareMap.dcMotor.get("motorWinchUp");
+        motorWinchDown = hardwareMap.dcMotor.get("motorWinchDown");
+
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorWinchUp.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorWinchDown.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorWinchUp.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorWinchDown.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        stopAllMotors();
+
+        telemetry.addData("Motor Initialization Complete", "");
+
+        servoMarker = hardwareMap.servo.get("servoMarker");
+        servoMarker.setPosition(0);
+
+        telemetry.addData("Servo Initialization Complete", "");
+
+        imu = new IMU(hardwareMap.get(BNO055IMU.class, "imu"));
+        imu.IMUinit(hardwareMap);
+
+        telemetry.addData("IMU Initialization Complete", "");
+
+        telemetry.addData("Initialization Complete", "");
+    }
+
+    public void stopDriveMotors() {
+        motorFR.setPower(0);
+        motorFL.setPower(0);
+        motorBR.setPower(0);
+        motorBL.setPower(0);
+    }
+
+    public void stopAllMotors() {
+        motorFR.setPower(0);
+        motorFL.setPower(0);
+        motorBR.setPower(0);
+        motorBL.setPower(0);
+
+        motorWinchDown.setPower(0);
+        motorWinchUp.setPower(0);
+    }
+    public void release() throws InterruptedException{
+        //lower the robot??
+        motorWinchDown.setPower(winchDownPower);
+        Thread.sleep(400); // we might wanna PID this
+    }
+}
