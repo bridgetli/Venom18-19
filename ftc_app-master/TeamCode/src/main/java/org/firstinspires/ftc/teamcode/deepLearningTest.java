2e71786e30f7da19b4a686b7fdd5d0187ceef9c0
Hamza A <hamzaimran2011@gmail.com>
2018-09-13 14:06:17 -0500

    Problem still persists.
    Seems to be caused by the external library 'com.google.code.gson:gson:2.7@jar'. The exact same jar file exists in 'RobotCore-release'. The solution would be to remove the former; however, I'm not sure how exactly to do that, since I never added it in the first place. It's clearly part of one of the dependancies I imported to implement DL4J, so the hard part is figuring out which one it comes from, and how to exclude that part of the dependancy from the project.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/deepLearningTest.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/deepLearningTest.java
@@ -2,17 +2,25 @@ package org.firstinspires.ftc.teamcode;
 
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.io.ClassPathResource;
 
 public class deepLearningTest {
 
     public void main(String[] args) {
-        try {
-            String simpleMlp = new ClassPathResource("model.h5").getFile().getPath();
-            MultiLayerNetwork model = KerasModelImport.importKerasSequentialModelAndWeights(simpleMlp);
-        } catch(Exception e) {
-            System.out.print("Exception: " + e.toString());
+        MultiLayerNetwork model = null;
+        while (model == null) {
+            try {
+                String simpleMlp = new ClassPathResource("model.h5").getFile().getPath();
+                model = KerasModelImport.importKerasSequentialModelAndWeights(simpleMlp);
+            } catch (Exception e) {
+                System.out.print("Exception: " + e.toString() + " try again.");
+            }
         }
+
+        INDArray input = Nd4j.create();
+        INDArray output = model.output(input);
 
     }
 }
