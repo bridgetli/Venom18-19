9ac4e5bc033dccafd78c3686e5f0543aa885ebc9
ShreyasK14 <40637461+ShreyasK14@users.noreply.github.com>
2018-09-20 17:23:22 -0500

    Made a right turn method

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDtesting.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDtesting.java
@@ -1,15 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by bodeng on 9/19/18.
  */
 
 public class PIDtesting extends CustomLinearOpMode {
 
+
     public void rightTurn(double angle) {
-        double kP = 0.5;
-        double currentAngle = angle - imu.getYaw();
-        double P = kP * currentAngle;
+        double kP = 0.2;
+        double kI = 0.001;
+        double kD = 0.005;
+        double angleError = 0;
+        double oldTime = 0;
+        double totalError = 0;
+        double oldError = 0;
+        double newTime = 0;
+        double I = 0;
+        double P = 0;
+        double D = 0;
+        ElapsedTime timeStuff = new ElapsedTime();
+        while (angleError > 0) {
+            angleError = angle - imu.getYaw();
+            newTime = timeStuff.milliseconds();
+            totalError += (newTime - oldTime) * angleError;
+            P = kP * angleError;
+            I = totalError * kI * angleError;
+            D = (angleError - oldError) / (newTime - oldTime) * kD;
+
+            oldTime = newTime;
+            oldError = angleError;
+            motorFL.setPower(Range.clip(P + I + D, -1, 1));
+            motorBL.setPower(Range.clip(P + I + D, -1, 1));
+            motorFR.setPower(Range.clip(-P - I - D, -1, 1));
+            motorBR.setPower(Range.clip(-P - I - D, -1, 1));
+        }
 
 
     }
@@ -18,10 +46,6 @@ public class PIDtesting extends CustomLinearOpMode {
     public void runOpMode() {
         initizialize();
         waitForStart();
-        motorFL.setPower(1);
-        motorBL.setPower(1);
-        motorFR.setPower(-1);
-        motorBR.setPower(-1);
         rightTurn(30);
     }
 }
