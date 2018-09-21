5051665c10fbda0b6e4c5b03d070928c285129c2
Summer Ely <summerely@gmail.com>
2018-09-21 08:12:42 -0500

    Converted pseudocode to actual code for Trollbot autonomous

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotAuto.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotAuto.java
@@ -71,35 +71,47 @@ public class TrollbotAuto extends CustomLinearOpMode {
             depositMarker();
             Turn(180.0);
             goForward(48.0);
+
+            if (isBlock2onLeft){
+                Turn(90.0);
+                goForward(12.0);
+                Turn(-90.0);
+                goForward(36.0);
+                // You are on the crater wall
+                //You could turn around a little more
+                // than 45 degrees right or something and go like
+                //12 sqrt 2 inches to be around the center of the crater,
+                // could turn another 90 degrees right to be
+                // facing the rover
+            }
+
+            else if (isBlock2onRight){
+                Turn(90.0);
+                goForward(36.0);
+                Turn(-90.0);
+                goForward(60.0);
+                // You are on the crater wall
+                //You could turn a little less than 45 degrees right
+                // and go backwards around 24 sqrt 2 inches to be
+                // around the center of the crater, could turn another
+                // 90 degrees right to be facing the rover
+            }
+
+            else if (isBlock2onCenter){
+                Turn(90.0);
+                goForward(24.0);
+                Turn(-90.0);
+                goForward(48.0);
+                // You are on the crater wall
+                //You could turn a little less than 45 degrees
+                // right and go backwards around 12 sqrt 2 inches
+                // to be around the center of the crater, could
+                // turn another 90 degrees right to be facing the rover
+            }
+
         }
-        else if (isBlock2onLeft == true){
-            Turn(90.0);
-            goForward(12.0);
-            Turn(-90.0);
-            goForward(36.0);
-            depositMarker();
-            Turn(180.0);
-            goForward(48.0);
-        }
-        else if (isBlock2onRight == true){
-            Turn(90.0);
-            goForward(36.0);
-            Turn(-90.0);
-            goForward(60.0);
-            depositMarker();
-            Turn(180.0);
-            goForward(48.0);
-        }
-        else if (isBlock2onCenter == true){
-            Turn(90.0);
-            goForward(24.0);
-            Turn(-90.0);
-            goForward(48.0);
-            depositMarker();
-            Turn(180.0);
-            goForward(48.0);
-        }
-        else if (isBlock1onRight == true){
+
+        if (isBlock1onRight){
             goForward(25.4558);
             Turn(-45.0);
             goForward(24.0);
@@ -109,9 +121,90 @@ public class TrollbotAuto extends CustomLinearOpMode {
             depositMarker();
             Turn(180.0);
             goForward(48.0);
+
+            if (isBlock2onLeft){
+                Turn(90.0);
+                goForward(12.0);
+                Turn(-90.0);
+                goForward(36.0);
+                // You are on the crater wall
+                //You could turn around a little more than 45
+                // degrees right or something and go like 12
+                // sqrt 2 inches to be around the center of the crater,
+                // could turn another 90 degrees right to be facing the rover
+            }
+
+            else if (isBlock2onRight){
+                Turn(90.0);
+                goForward(36.0);
+                Turn(-90.0);
+                goForward(60.0);
+                // You are on the crater wall
+                // You could turn a little less than 45 degrees right
+                // and go backwards around 24 sqrt 2 inches to be around
+                // the center of the crater, could turn another 90 degrees
+                // right to be facing the rover
+            }
+
+            else if (isBlock2onCenter){
+                Turn(90.0);
+                goForward(24.0);
+                Turn(-90.0);
+                goForward(48.0);
+                // You are on the crater wall
+                //You could turn a little less than 45 degrees right
+                // and go backwards around 12 sqrt 2 inches to be
+                // around the center of the crater, could turn another
+                // 90 degrees right to be facing the rover
+            }
+
         }
 
-        else if (isBlock1onCenter){
+        if (isBlock1onCenter){
+            goForward(59.397);
+            Turn(45.0);
+            goForward(12.0);
+            Turn(-90.0);
+            depositMarker();
+            Turn(180);
+            goForward(48.0);
+
+            if (isBlock2onLeft){
+                Turn(90);
+                goForward(12.0);
+                Turn(-90.0);
+                goForward(36.0);
+                // You are on the crater wall
+                //You could turn around a little more than
+                // 45 degrees right or something and go like
+                //12 sqrt 2 inches to be around the center of the crater,
+                // could turn another 90 degrees right to be facing the rover
+            }
+
+            else if (isBlock2onRight){
+                Turn(90);
+                goForward(36.0);
+                Turn(-90.0);
+                goForward(60.0);
+                // You are on the crater wall
+                //You could turn around a little more than
+                // 45 degrees right or something and go like
+                //12 sqrt 2 inches to be around the center of the
+                // crater, could turn another 90 degrees right to be
+                // facing the rover
+            }
+
+            else if (isBlock2onCenter){
+                Turn(90.0);
+                goForward(24.0);
+                Turn(-90.0);
+                goForward(48.0);
+                // You are on the crater wall
+                //You could turn a little less than 45 degrees
+                // right and go backwards around 12 sqrt 2 inches
+                // to be around the center of the crater,
+                // could turn another 90 degrees right to be facing the rover
+            }
 
         }
 
