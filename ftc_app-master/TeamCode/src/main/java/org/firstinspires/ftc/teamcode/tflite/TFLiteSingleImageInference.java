d55f6acce98ad92455afab63f4c081f832abbf4b
HamzaAli1 <hamzaimran2011@gmail.com>
2018-11-28 17:26:07 -0600

    plz test this later

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tflite/TFLiteSingleImageInference.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tflite/TFLiteSingleImageInference.java
@@ -4,55 +4,70 @@ import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
 import org.firstinspires.ftc.teamcode.CustomLinearOpMode;
 import org.tensorflow.lite.Interpreter;
-
 import java.io.FileInputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.Arrays;
 
 @Autonomous(name = "TFLiteSingleImageInference", group = "test")
 public class TFLiteSingleImageInference extends CustomLinearOpMode {
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        float[][] result = new float[1][10];
+    public void runOpMode() {
+        float[][] probabilities = new float[1][10];
         Bitmap bmp;
         ByteBuffer buffer;
         Interpreter tflite;
+        StringBuilder results;
+
+        try {
+            bmp = BitmapFactory.decodeStream(hardwareMap.appContext.getAssets().open("fashion-mnist-sprite.png"));
+            buffer = convertBitmapToByteBuffer(bmp);
+            tflite = new Interpreter(loadModelFile(hardwareMap.appContext)); //TODO test if this works as read-only buffer
+        } catch (Exception e) {
+            telemetry.addData("ERROR", e.toString());
+            return;
+        }
 
         telemetry.addData("Press Play to Start", "Tensorflow Inference");
         telemetry.update();
         waitForStart();
 
         while(opModeIsActive()) {
+            results = new StringBuilder();
             try {
-                bmp = BitmapFactory.decodeStream(hardwareMap.appContext.getAssets().open("fashion-mnist-sprite.png"));
-                buffer = convertBitmapToByteBuffer(bmp);
-                tflite = new Interpreter(loadModelFile(hardwareMap.appContext));
+                tflite.run(buffer, probabilities);
+                for (int i = 0; i < 10; i++) {
+                    results.append("Label ").append(i).append(": ").append(probabilities[0][i]).append("\n");
+                }
 
-                tflite.run(buffer, result);
-                telemetry.addData("Results", Arrays.toString(result));
+                telemetry.addData("RESULTS", results.toString());
             } catch (Exception e) {
                 telemetry.addData("ERROR", e.toString());
             }
             telemetry.update();
         }
+        tflite.close();
     }
 
     private MappedByteBuffer loadModelFile(Context activity) throws Exception {
+        MappedByteBuffer model;
         AssetFileDescriptor fileDescriptor = activity.getAssets().openFd("roboModel.tflite");
         FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
         FileChannel fileChannel = inputStream.getChannel();
         long startOffset = fileDescriptor.getStartOffset();
         long declaredLength = fileDescriptor.getDeclaredLength();
-        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+        model = fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+
+        fileDescriptor.close();
+        inputStream.close();
+        fileChannel.close();
+
+        return model;
     }
 
     private ByteBuffer convertBitmapToByteBuffer(Bitmap bitmap) {
@@ -64,9 +79,9 @@ public class TFLiteSingleImageInference extends CustomLinearOpMode {
         for (int i = 0; i < 28; ++i) {
             for (int j = 0; j < 28; ++j) {
                 final int val = intValues[pixel++];
-                byteBuffer.put((byte) ((val >> 16) & 0xFF));
-                byteBuffer.put((byte) ((val >> 8) & 0xFF));
-                byteBuffer.put((byte) (val & 0xFF));
+                byteBuffer.putFloat(((val >> 16) & 0xFF));
+                byteBuffer.putFloat(((val >> 8) & 0xFF));
+                byteBuffer.putFloat((val & 0xFF));
             }
         }
         return byteBuffer;
