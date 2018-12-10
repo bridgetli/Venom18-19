fec8a61752616a1070a2e6a3140edcc387dd0bf5
HamzaAli1 <hamzaimran2011@gmail.com>
2018-12-10 16:07:44 -0600

    time to take a break
    seriously, this is not going to work

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tflite/TFLiteSingleImageInference.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tflite/TFLiteSingleImageInference.java
@@ -5,85 +5,83 @@ import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.teamcode.CustomLinearOpMode;
 import org.tensorflow.lite.Interpreter;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Arrays;
 
 @Autonomous(name = "TFLiteSingleImageInference", group = "test")
 public class TFLiteSingleImageInference extends CustomLinearOpMode {
 
-    @Override
-    public void runOpMode() {
-        float[][] probabilities = new float[1][10];
-        Bitmap bmp;
-        ByteBuffer buffer;
-        Interpreter tflite = null;
+    private MappedByteBuffer tfliteModel;
+    private Interpreter tflite;
+    private ByteBuffer imgData = null;
+    private int[] intValues;
+    private float[][] labelProbArray = null;
+    private VuforiaLocalizer vuforia;
+    private String MODEL_NAME = "roboModel.tflite";
+    private String IMAGE_FILE = "fashion-mnist-sprite-GRAY.png";
 
-        telemetry.addData("Press Play to Start", "Tensorflow Inference");
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addLine("Press PLAY to start");
         telemetry.update();
         waitForStart();
 
+        try {
+            tfliteModel = loadModelFile(hardwareMap.appContext.getApplicationContext());
+            tflite = new Interpreter(tfliteModel);
+
+            Bitmap bmp = BitmapFactory.decodeStream(hardwareMap.appContext.getAssets().open(IMAGE_FILE));
+            convertBitmapToByteBuffer(bmp);
+            telemetry.addLine("Model & Image Successfully Loaded");
+        } catch (Exception e) {
+            telemetry.addData("Model not loaded", e.toString());
+        }
+        telemetry.update();
+
         while(opModeIsActive()) {
             try {
-                bmp = BitmapFactory.decodeStream(hardwareMap.appContext.getAssets().open("fashion-mnist-sprite-GRAY.png"));
-                buffer = convertBitmapToByteBuffer(bmp);
-                tflite = new Interpreter(loadModelFile(hardwareMap.appContext)); //TODO test if this works as read-only buffer
-
-                tflite.run(buffer, probabilities);
-                for (int i = 0; i < 10; i++)
-                    telemetry.addData("Label " + i, probabilities[0][i]);
+                tflite.run(imgData, labelProbArray);
+                telemetry.addData("Results", Arrays.toString(labelProbArray));
             } catch (Exception e) {
-                telemetry.addData("ERROR", e.toString());
+                telemetry.addData("Couldn't run inference", e.toString());
             }
             telemetry.update();
         }
-        if (tflite != null)
-            tflite.close();
+        tflite.close();
     }
 
-    private MappedByteBuffer loadModelFile(Context activity) throws Exception {
-        MappedByteBuffer model;
-        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd("roboModel.tflite");
+    private MappedByteBuffer loadModelFile(Context activity) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(MODEL_NAME);
         FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
         FileChannel fileChannel = inputStream.getChannel();
         long startOffset = fileDescriptor.getStartOffset();
         long declaredLength = fileDescriptor.getDeclaredLength();
-        model = fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
-
-        fileDescriptor.close();
-        inputStream.close();
-        fileChannel.close();
-
-        return model;
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
     }
 
-    private ByteBuffer convertBitmapToByteBuffer(Bitmap bm) {
-        //create byte buffer
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(28 * 28 * 4);
-        byteBuffer.order(ByteOrder.nativeOrder());
-        int[] intValues = new int[28 * 28];
-        //transfer data
-        byteBuffer.rewind();
-        bm.getPixels(intValues, 0, bm.getWidth(), 0, 0, bm.getWidth(), bm.getHeight());
+    private void convertBitmapToByteBuffer(Bitmap bitmap) {
+        if (imgData == null) {
+            return;
+        }
+        imgData.rewind();
+        intValues = new int[28*28];
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        // Convert the image to floating point.
         int pixel = 0;
         for (int i = 0; i < 28; ++i) {
             for (int j = 0; j < 28; ++j) {
                 final int val = intValues[pixel++];
-                byteBuffer.putFloat(((val >> 16) & 0xFF));
-                byteBuffer.putFloat(((val >> 8) & 0xFF));
-                byteBuffer.putFloat((val & 0xFF));
+                imgData.putFloat(((val >> 16) & 0xFF) / 255.f);
+                imgData.putFloat(((val >> 8) & 0xFF) / 255.f);
+                imgData.putFloat((val & 0xFF) / 255.f);
             }
         }
-        return byteBuffer;
     }
-
-    /*
-    byteBuffer.put((byte) ((val >> 16) & 0xFF));
-    byteBuffer.put((byte) ((val >> 8) & 0xFF));
-    byteBuffer.put((byte) (val & 0xFF));
-    */
 }
